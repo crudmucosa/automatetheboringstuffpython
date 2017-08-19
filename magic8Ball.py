5805839f9e1770e92776ba647d3f8b5cfe10810b
Desmond Johnson <fenris@cad-bane.home>
2017-08-18 20:00:30 -0400

    added 10th option in magic eight ball

--- magic8Ball.py
+++ magic8Ball.py
@@ -4,13 +4,13 @@ import random
 
 def getAnswer(answerNumber):
     if answerNumber == 1:
-        return 'It is written'
+        return 'NO'
     elif answerNumber == 2:
         return 'Oh most definitely'
     elif answerNumber == 3:
         return 'Sure, why not?'
     elif answerNumber == 4:
-        return 'All aboard the hazy train!'
+        return 'YES'
     elif answerNumber == 5:
         return 'Why not ask again?'
     elif answerNumber == 6:
@@ -18,10 +18,13 @@ def getAnswer(answerNumber):
     elif answerNumber == 7:
         return 'Nope. No fricking way.'
     elif answerNumber == 8:
-        return 'Prospects aren''t great'
+        return 'Prospects aren\'t great'
     elif answerNumber == 9:
         return 'Doubtful'
+    elif answerNumber == 10:
+        return 'Doubtful'
 
-r = random.randint(1, 9)
+r = random.randint(1, 10)
 fortune = getAnswer(r)
+#print('The number is ' + str(r))
 print(fortune)
