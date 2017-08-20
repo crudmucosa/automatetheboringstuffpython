150676be57a1b1bf098ca86c4a3665ec147f7997
Desmond Johnson <fenris@cad-bane.home>
2017-08-20 07:16:07 -0400

    doing cool list tricks

---
+++ messingWithLists.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+# Playing around with lists
+
+coolList = ['murder ball', 'bunny slippers', 'death star', 'canon']
+
+print len(coolList)
+
+print coolList[:2] #print the first 2 elements of the list
+
+coolList[-1] = 331133 #change the last element in the list to the value
+
+print coolList
